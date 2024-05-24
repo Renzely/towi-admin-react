96e0944042bb6020e41da5515514d15a3063caa4
Renzely <ynsonharold@gmail.com>
2024-05-24 13:44:49 +0800

    main

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.css";
+import "./defaultApp.css";
+import Attendance from "./components/pages/attendance/Attendance";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+} from "react-router-dom";
+import Parcel from "./components/pages/parcel/Parcel";
+import ViewAttendance from "./components/pages/attendance/ViewAttendance";
+import Account from "./components/pages/account/Account";
+import ViewParcel from "./components/pages/parcel/ViewParcel";
+
+function App() {
+  return (
+    <div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+
+        <Routes>
+          <Route path="/" element={<Attendance />} />
+          <Route path="/parcel" element={<Parcel />} />
+          <Route path="/view-parcel" element={<ViewParcel />} />
+          <Route path="/view-attendance" element={<ViewAttendance />} />
+          <Route path="/view-accounts" element={<Account />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
